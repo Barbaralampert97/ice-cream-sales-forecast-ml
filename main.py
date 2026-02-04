d8406ded8093be051840dd57072b3d8c32517ec5
Bárbara Lampert <lampertbarbara97@gmail.com>
2026-02-04 11:48:53 -0300

    Implementa modelo de regressão linear para previsão de vendas

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv('data/Ice Cream.csv')
 
@@ -13,3 +14,13 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 
 print('Tamanho do conjunto de treino:', X_train.shape)
 print ('Tamanho do conjunto de teste: ', X_test.shape)
+
+# Criando o modelo de Regressão Linear
+model = LinearRegression()
+
+# Treinando o modelo
+model.fit(X_train, y_train)
+
+# o predict pega cada valor do X_test e vai colocar na formula que o modelo aprendeu no treino:
+y_pred = model.predict(X_test)
+print(y_pred)
